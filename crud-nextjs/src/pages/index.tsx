df89cb4b72fd7883943b012895d6f692611a14d7
Kaian Lemos <klemos@minsait.com>
2023-03-19 16:24:23 -0300

    criacao da classe Cliente e components Tabela, Layout e Titulo

--- crud-nextjs/src/pages/index.tsx
+++ crud-nextjs/src/pages/index.tsx
@@ -1,14 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 20, '2'),
+    new Cliente('Carlos', 40, '3'),
+    new Cliente('Pedro', 16, '4')
+  ]
+
   return (
-    <div className='flex '>
-      <span>Texto</span>
+    <div className={
+      `flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-600
+      text-white
+      `}>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
     </div>
   )
 }
